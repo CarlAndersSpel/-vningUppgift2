eb720067222499dd55c8d9f8a9f12284fbc022b4
CarlAndersSpel <caar5853@student.su.se>
2026-04-09 13:41:38 +0200

    Update Recording.java

--- -vningUppgift2/prog2-ovning2/src/main/java/se/su/ovning2/Recording.java
+++ -vningUppgift2/prog2-ovning2/src/main/java/se/su/ovning2/Recording.java
@@ -1,14 +1,18 @@
 package se.su.ovning2;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
+//se till att den här klassen ska gå till hashmapen i Exercicetest2.
 
+//ska överskugga två metoder.
 public class Recording {
   private final int year;
   private final String artist;
   private final String title;
   private final String type;
   private final Set<String> genre;
+  //private final Map<String> key = new HashMap<>();
 
   public Recording(String title, String artist, int year, String type, Set<String> genre) {
     this.title = title;
@@ -16,6 +20,7 @@ public class Recording {
     this.artist = artist;
     this.type = type;
     this.genre = genre;
+    
   }
 
   public String getArtist() {
@@ -42,4 +47,21 @@ public class Recording {
   public String toString() {
     return String.format("{ %s | %s | %s | %d | %s }", artist, title, genre, year, type);
   }
+
+  @Override
+  public boolean equals(Object obj){
+//är de samma t.ex. year =  year
+        if (this == obj){
+          return true;
+        }
+        Recording that = (Recording) obj;
+        return year == that.year && artist == that.artist && title == that.title;
+}
+
+@Override
+public int hashCode(){
+   return Objects.hash(year, artist, title);
+}
+
+
 }
